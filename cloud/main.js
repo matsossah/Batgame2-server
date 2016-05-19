7591405015c6a8be868bc02407cfb5a3808d56ee
Morhaus <alexandre.kirszenberg@gmail.com>
2016-05-19 16:45:29 +0200

    Set hasUsername field on save

---
+++ cloud/main.js
@@ -0,0 +1,8 @@
+Parse.Cloud.beforeSave(Parse.User, (request, response) => {
+  if (request.object.isNew()) {
+    const username = request.object.get('username');
+    request.object.set('hasUsername', username !== undefined);
+  }
+  // @TODO: Check whether user is allowed to modify its username
+  response.success();
+});
